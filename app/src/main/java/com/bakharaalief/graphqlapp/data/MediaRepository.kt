700946b244a395570bf1565418f52a6623af32ef
bakharaalief <50726376+bakharaalief@users.noreply.github.com>
2022-09-01 09:46:19 +0700

    new api for data

--- app/src/main/java/com/bakharaalief/graphqlapp/data/MediaRepository.kt
+++ app/src/main/java/com/bakharaalief/graphqlapp/data/MediaRepository.kt
@@ -3,16 +3,16 @@ package com.bakharaalief.graphqlapp.data
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
 import com.apollographql.apollo3.ApolloClient
-import com.bakharaalief.app.MediaListQuery
+import com.bakharaalief.app.CharactersQuery
 
 class MediaRepository(private val client: ApolloClient) {
 
-    fun getMedia(): LiveData<Resource<List<MediaListQuery.Medium?>?>> = liveData {
+    fun getMedia(): LiveData<Resource<List<CharactersQuery.Result?>?>> = liveData {
         emit(Resource.Loading)
 
         try {
-            val response = client.query(MediaListQuery()).execute()
-            val data = response.data?.Page?.media
+            val response = client.query(CharactersQuery()).execute()
+            val data = response.data?.characters?.results
             emit(Resource.Success(data))
         } catch (e: Exception) {
             emit(Resource.Error(e.message.toString()))
