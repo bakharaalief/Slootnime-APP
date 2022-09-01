f078332f28201fd1261e14e4fd6287901fa5aa84
Bakhara Alief <ara.bakhara12@gmail.com>
2022-09-01 16:42:24 +0700

    add characters detail (#4)

--- app/src/main/java/com/bakharaalief/graphqlapp/data/CharacterRepository.kt
+++ app/src/main/java/com/bakharaalief/graphqlapp/data/CharacterRepository.kt
@@ -1,11 +1,14 @@
 package com.bakharaalief.graphqlapp.data
 
+import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.liveData
 import com.apollographql.apollo3.ApolloClient
+import com.bakharaalief.app.CharactersByIdsQuery
 import com.bakharaalief.app.CharactersQuery
 import com.bakharaalief.graphqlapp.data.network.CharactersPagingSource
 
@@ -21,4 +24,21 @@ class CharacterRepository(private val client: ApolloClient) {
             }
         ).liveData
     }
+
+    fun getCharactersByIds(id: String): LiveData<Resource<CharactersByIdsQuery.CharactersById?>> =
+        liveData {
+            emit(Resource.Loading)
+
+            try {
+                val array = ArrayList<String>()
+                array.add(id)
+                val response = client.query(CharactersByIdsQuery(array)).execute()
+                val character = response.data?.charactersByIds?.filterNotNull()?.get(0)
+
+                emit(Resource.Success(character))
+            } catch (e: Exception) {
+                Log.d("test", "$e")
+                emit(Resource.Error(e.toString()))
+            }
+        }
 }
