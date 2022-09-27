8360322bdfaca7dd4de62e8991c2496e29230957
bakharaalief <50726376+bakharaalief@users.noreply.github.com>
2022-09-27 12:28:02 +0700

    add character in media detail

--- app/src/main/java/com/bakharaalief/graphqlapp/util/DataMapper.kt
+++ app/src/main/java/com/bakharaalief/graphqlapp/util/DataMapper.kt
@@ -2,10 +2,7 @@ package com.bakharaalief.graphqlapp.util
 
 import com.bakharaalief.app.ListMediaQuery
 import com.bakharaalief.app.MediaByIdQuery
-import com.bakharaalief.graphqlapp.domain.model.Date
-import com.bakharaalief.graphqlapp.domain.model.Media
-import com.bakharaalief.graphqlapp.domain.model.MediaById
-import com.bakharaalief.graphqlapp.domain.model.Staff
+import com.bakharaalief.graphqlapp.domain.model.*
 
 object DataMapper {
 
@@ -38,13 +35,24 @@ object DataMapper {
             this.title?.native ?: "not found",
             this.title?.romaji ?: "not found",
             this.description ?: "not found",
-            this.staff?.nodes?.filterNotNull()?.toStaffModel() ?: emptyList()
+            this.staff?.nodes?.filterNotNull()?.toStaffModel() ?: emptyList(),
+            this.characters?.nodes?.filterNotNull()?.toCharacterModel() ?: emptyList()
         )
     }
 
     private fun List<MediaByIdQuery.Node>.toStaffModel(): List<Staff> {
         return this.map {
             Staff(
+                it.id,
+                it.name?.full ?: "not found",
+                it.image?.large ?: "not found"
+            )
+        }
+    }
+
+    private fun List<MediaByIdQuery.Node1>.toCharacterModel(): List<Character> {
+        return this.map {
+            Character(
                 it.id,
                 it.name?.full ?: "not found",
                 it.image?.large ?: "not found"
